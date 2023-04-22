5d4f2bf39ebdfe6599c0140f03b5c73f9962c81d
TheGrantalope <126519462+TheGrantalope@users.noreply.github.com>
2023-04-22 14:42:42 -0400

    Revert "Merge branch 'main' of https://github.com/TheGrantalope/Those-That-Shape-Us"
    This reverts commit e61cebc195651812536077f65ba4611fef68c3cc, reversing
    changes made to a19728aa2feea54ab8c1c11d9cead12fc910ab5a.

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -10,12 +10,6 @@ public class MusicController : MonoBehaviour
     private float rate = 270.0f;
     private AudioSource music;
 
-    // Public Variables
-    public AudioClip[] idleMusic;
-    public AudioClip[] battleMusic;
-    public GameObject player;
-    public GameObject slime;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -27,21 +21,19 @@ public class MusicController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // IdleMusic is called in InvokeRepeating 
     void IdleMusic()
     {
         delay = Random.Range(delayRange[0], delayRange[1]);
-        music.clip = idleMusic[Random.Range(0, idleMusic.Length)];
         music.PlayDelayed(delay);
     }
 
     void BattleMusic()
     {
-        music.clip = battleMusic[Random.Range(0, battleMusic.Length)];
-        music.Play();
+
     }
 
 }
