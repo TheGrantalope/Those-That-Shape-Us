301dfdeaedb2b8fc7b2add42503715fb68c7e07c
amanilester <118773252+amanilester@users.noreply.github.com>
2023-04-24 19:44:09 -0400

    Slime Movement Adjustments

--- Assets/Scripts/SlimeMovement.cs
+++ Assets/Scripts/SlimeMovement.cs
@@ -1,48 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class SlimeMovement : MonoBehaviour
 {
-    public float speed;
-    public Transform[] patrol;
-    public float waitTime;
-    int currentPoint;
-    bool wait_once_flag;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float speed = 3f;
+    public DetectionZone attackZone;
+    Rigidbody2D rb;
+    TouchingDirections touchingDirections;
+    Animator animator;
 
+    public enum DirectionFace { Right, Left}
+
+    private DirectionFace _direction;
+    private Vector2 directionVector = Vector2.right;
+    private DirectionFace Direction
+    {
+        get { return _direction; }
+        set
+        {
+            if ( _direction != value )
+            {
+                // flip direction
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+
+                if(value == DirectionFace.Right)
+                {
+                    directionVector = Vector2.right;
+                }
+                else if(value == DirectionFace.Left)
+                {
+                    directionVector = Vector2.left;
+                }
+            }
+
+            _direction = value; }
+    }
+
+    public bool _hasTarget = false;
+
+    public bool HasTarget
+    {
+        get { return _hasTarget; }
+        private set
+        {
+            _hasTarget = value;
+            animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        touchingDirections = GetComponent<TouchingDirections>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position != patrol[currentPoint].position)
+        HasTarget = attackZone.detectedColliders.Count > 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if(touchingDirections.IsGrounded && touchingDirections.IsOnWall)
         {
-            transform.position = Vector2.MoveTowards(transform.position, patrol[currentPoint].position, speed * Time.deltaTime);
+            FlipDirection();
+        }
+        rb.velocity = new Vector2(speed * directionVector.x, rb.velocity.y);
+    }
+
+    private void FlipDirection()
+    {
+        if(Direction == DirectionFace.Right)
+        {
+            Direction = DirectionFace.Left;
+        }
+        else if(Direction == DirectionFace.Left)
+        {
+            Direction = DirectionFace.Right;
         }
         else
         {
-            if (wait_once_flag == false)
-            {
-                wait_once_flag = true;
-                StartCoroutine(Wait());
-            }
+            Debug.LogError("Current irection face is not set to legal values of right or left");
         }
     }
 
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(waitTime);
-        if (currentPoint + 1 < patrol.Length)
-        {
-            currentPoint++;
-        }
-        else
-        {
-            currentPoint = 0;
-        }
-        wait_once_flag = false;
-    }
-}
+}   
