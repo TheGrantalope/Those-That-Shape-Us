63b399ae480b592749fcf68fb0b12dbd86045640
TheGrantalope <126519462+TheGrantalope@users.noreply.github.com>
2023-04-26 04:36:41 -0400

    Revert "Merge"
    This reverts commit 1a05052534a703880d183873be82b8b9a0fc1586, reversing
    changes made to de01c5168fd13f7dc3ce721b83a51a4cf58497f5.

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -4,102 +4,36 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
-    // Audio Management
-    public AudioClip[] idleClip;
-    public AudioClip battleClip;
+    // Private Variables
+    private float[] delayRange = { 5.0f, 45.0f };
+    private float delay;
+    private float rate = 270.0f;
     private AudioSource music;
-    private float[] delayRange = { 45.0f, 46.0f };
-
-    // Fade Management
-    private float threshold;
-    private float minVolume = 0;
-    private float maxVolume = 0.75f;
-    private float timeDuration = 5.0f;
-    private float timeElapsed = 0.0f;
-    private float clipOffset = 5.0f;
-    private bool fade;
 
     // Start is called before the first frame update
     void Start()
     {
         music = GetComponent<AudioSource>();
-        float delay = Random.Range(delayRange[0], delayRange[1]);
-        InvokeRepeating("IdleMusic", delay, delayRange[0]);
+        delay = Random.Range(delayRange[0], delayRange[1]);
+        InvokeRepeating("IdleMusic", delay, rate);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fade)
-        {
-            FadeIn();
-        } else if (music.time >= threshold)
-        {
-            FadeOut();
-        }
-
-        if (gameObject.GetComponent<FindClosestSlime>().FindClosestEnemy() != null)
-        {
-            if (music.clip != battleClip)
-            {
-                BattleMusic();
-            }
-        }
 
     }
 
-    void FadeIn()
-    {
-        if (music.isPlaying)
-        {
-            music.volume = Mathf.Lerp(minVolume, maxVolume, timeElapsed / timeDuration);
-            timeElapsed += Time.deltaTime;
-        }
-        if (music.volume == maxVolume)
-        {
-            timeElapsed = 0.0f;
-            fade = false;
-        }
-    }
-
-    void FadeOut()
-    {
-        if (music.isPlaying)
-        {
-            music.volume = Mathf.Lerp(maxVolume, minVolume, timeElapsed / timeDuration);
-            timeElapsed += Time.deltaTime;
-        }
-        if (music.volume == minVolume)
-        {
-            timeElapsed = 0.0f;
-            music.Stop();
-            fade = true;
-        }
-    }
-
+    // IdleMusic is called in InvokeRepeating 
     void IdleMusic()
     {
-        if (!music.isPlaying)
-        {
-            fade = true;
-            float delay = Random.Range(delayRange[0], delayRange[1]);
-            music.clip = idleClip[Random.Range(0, idleClip.Length)];
-            threshold = music.clip.length - clipOffset;
-            music.Play();
-        }   
+        delay = Random.Range(delayRange[0], delayRange[1]);
+        music.PlayDelayed(delay);
     }
 
     void BattleMusic()
     {
-        if (music.isPlaying && music.clip != battleClip)
-        {
-            FadeOut();
-        } else
-        {
-            music.clip = battleClip;
-            threshold = music.clip.length - clipOffset;
-            music.Play();
-        }   
+
     }
 
 }
